b2fdaef9ccbe6d7cd652d99ef9f4fc60d36d5d1c
Itzae Rivera <itzae@ives.edu.mx>
2022-09-08 19:54:08 -0500

    Inicilización proyecto django

---
+++ webDjango/view.py
@@ -0,0 +1,26 @@
+from pydoc import doc
+from xml.dom.minidom import Document
+from django.http import HttpResponse 
+from django.template import loader
+from AppCoder.models import Curso
+
+def home(self, name):
+    return HttpResponse(f'Hola soy {name}')
+
+def homePage(self):
+    lista = [1,2,3,4,5]
+    data = {'nombre':'Itzae','apellido':'Rivera', 'lista':lista}
+    planilla = loader.get_template('home.html')
+    documento = planilla.render(data)
+    return HttpResponse(documento)
+
+def cursos(self):
+    #planilla = loader.get_template('cursos.html')
+    curso = Curso(nombre="UX/UI", camada="12345")
+    curso.save()
+    
+    documento = f'Curso: {curso.nombre} camada: {curso.camada}' 
+    return HttpResponse(documento)
+
+
+
